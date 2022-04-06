35f058aef20e252217a8951f3194408df951bdf8
tparlmer <tparlmer@gmail.com>
2022-04-06 14:52:48 -0400

    added more tests

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -34,3 +34,9 @@ test("gets inventory from player or returns false", () => {
 
   expect(player.getInventory()).toEqual(false);
 });
+
+test("gets player's health value", () => {
+  const player = new Player('Dave');
+
+  expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
